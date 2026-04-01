f14da22d84ca859578b75085ea59210b727925eb
Said_Ahmed_1 <saed.ahmed.ai96@gmail.com>
2026-04-01 17:38:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://nexus-store2.netlify.app/",
+        "https://nexus-store2.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
